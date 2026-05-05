308998ab5351db431f4e81490b785ebc38240ef8
Haley Benson <hlb73@txstate.edu>
2026-05-04 20:11:19 -0500

    Question 3.4 - Solved

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -9,3 +9,12 @@ FROM account
 WHERE status = 'ACTIVE'
 AND avail_balance > 2500;
 
+-- Question 3.3 - Learning SQL 2nd ed.
+SELECT DISTINCT open_emp_id
+FROM account;
+
+-- Question 3.4 - Learning SQL 2nd ed.
+SELECT p.product_cd, a.cust_id, a.avail_balance
+FROM product p INNER JOIN account a
+    ON p.product_cd = a.product_cd
+WHERE p.product_type_cd = 'ACCOUNT';
