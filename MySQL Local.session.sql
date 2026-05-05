5c5c068108fb643ec076527f0ec0bc430108b1d1
Haley Benson <hlb73@txstate.edu>
2026-05-04 19:55:11 -0500

    Question 3.1 - solved

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -1,9 +1,11 @@
--- Question 3.2
+-- Question 3.1 - Learning SQL 2nd ed.
+SELECT emp_id, fname, lname
+FROM employee
+ORDER BY lname, fname;
+
+-- Question 3.2 - Learning SQL 2nd ed.
 SELECT account_id, cust_id, avail_balance
 FROM account
 WHERE status = 'ACTIVE'
 AND avail_balance > 2500;
 
--- Test
-SELECT *
-FROM employee;
