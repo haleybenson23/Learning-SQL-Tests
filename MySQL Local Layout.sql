ab70d0d6b2030eaba29ece62a13f9e9508fd3886
Haley Benson <hlb73@txstate.edu>
2026-05-04 19:33:00 -0500

    queries for quick table visibility

---
+++ MySQL Local Layout.sql
@@ -0,0 +1,33 @@
+-- Table Layouts
+SELECT *
+FROM account;
+
+SELECT *
+FROM branch;
+
+SELECT *
+FROM business;
+
+SELECT *
+FROM customer;
+
+SELECT *
+FROM department;
+
+SELECT *
+FROM employee;
+
+SELECT *
+FROM individual;
+
+SELECT *
+FROM officer;
+
+SELECT *
+FROM product;
+
+SELECT *
+FROM product_type;
+
+SELECT *
+FROM transaction;
